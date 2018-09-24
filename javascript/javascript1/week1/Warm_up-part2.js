6e2fa1eb7fb72219a411ed60d345cffe81ab38f1
Netabay <mnetabay125@gmail.com>
2018-09-24 19:00:34 +0200

    I have finished the warm up exercises

---
+++ javascript/javascript1/week1/Warm_up-part2.js
@@ -0,0 +1,28 @@
+console.log("ሰላም ዓለም")//tigrinya
+const number =7.25
+ console.log(parseInt(7.25));
+ //Array
+ const Arraylist=[];
+ console.log("I think the value of the array is string")
+ var myFavoriteAnimals=['cat','goat','monkey','birds'];
+ console.log(myFavoriteAnimals);
+ var myFavoriteAnimals=('cat','goat','monkey','birds','baby pig');
+ console.log(myFavoriteAnimals);
+ //string
+ const letmyString="this is a test";
+console.log(letmyString);
+console.log("this is a test".length);
+//working with variable type
+let foodMenu='lasagna';
+let lasagnaPrice=68;
+let drinkMenu='coca';
+let cocaPrice=30.5;
+let totalPrice;
+totalPrice=lasagnaPrice + cocaPrice;
+console.log('ordered food is ' +foodMenu,'& ', 'ordered drink is '+drinkMenu, 'all price is '+totalPrice );
+console.log('the type of my variable cocaPrice is float ')
+console.log( typeof cocaPrice);
+
+
+
+
