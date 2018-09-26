7af0df2f0b91e0f5d5b7821adb4b9ffccd2eb990
Netabay <mnetabay125@gmail.com>
2018-09-26 20:03:52 +0200

    dog_age_calculator.js

---
+++ javascript/javascript1/week1/dog_age_calculator.js
@@ -0,0 +1,13 @@
+const dogYearOfBirth =2020;
+let dogYearFuture= 2027;
+let dogYear;
+let dogYearHuman;
+let shouldShowResultInDogYears= false;
+dogYear=dogYearFuture-dogYearOfBirth;
+if(shouldShowResultInDogYears){
+    console.log('your dog will be ' +dogYear,' human years old by 2027 ');
+    }
+    else{
+        dogYearHuman=10*dogYear;
+        console.log('Your dog will be '+ dogYearHuman,'dog years old in 2027');
+    }
